3e93252a4875e3520bcf22b0012f7faedd101baa
Jennifer916 <jenniferchenxil2789@nycstudents.net>
2025-04-23 16:30:34 +0000

    2nd change

--- Book.java
+++ Book.java
@@ -3,7 +3,8 @@ private String title;
 private double price; 
 
 public Book (String bookTitle, double bookPrice) {
-
+    title = bookTitle ; 
+    price= bookPrice; 
 }
 public String getTitle() { 
     return title;
